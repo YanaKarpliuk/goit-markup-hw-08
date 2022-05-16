557a2e840fb167be8e74ec92945d93930b0e1c3a
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-05-16 20:44:27 +0300

    notification

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".menu-open-button"),
+    closeMenuBtn: document.querySelector(".menu-close-button"),
+    menu: document.querySelector(".mob-menu"),
+    body: document.querySelector(".body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
